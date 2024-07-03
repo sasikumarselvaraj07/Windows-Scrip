e2f5ec5fcfe2bc6be43aeaf256665783bb7f43ef
sasikumarselvaraj07 <153719664+sasikumarselvaraj07@users.noreply.github.com>
2024-07-03 08:25:06 +0530

    Add files via upload

---
+++ GPO_Fetch.ps1
@@ -0,0 +1,30 @@
+﻿$FolderPath = "$Env:USERPROFILE\Downloads\GPO\"
+
+# Check if the folder path exists
+if (-not (Test-Path -Path $FolderPath)) {
+    # If the folder path does not exist, create the folder
+    New-Item -Path $FolderPath -ItemType Directory -Force
+    Write-Host "Folder created at $FolderPath"
+} else {
+    Write-Host "Folder already exists at $FolderPath"
+}
+
+Get-GPOReport -All -ReportType Html -Path "$FolderPath\All-GPOs.html"
+
+# Retrieve all GPOs
+$AllGpos = Get-GPO -All
+
+# Create a custom object holding all the GPOs and their links (separate for each distinct OU)
+$GpoLinks = foreach ($g in $AllGpos){
+    [xml]$Gpo = Get-GPOReport -ReportType Xml -Guid $g.Id
+    foreach ($i in $Gpo.GPO.LinksTo) {
+        [PSCustomObject]@{
+            "Name" = $Gpo.GPO.Name
+            "Link" = $i.SOMPath
+            "LinkEnabled" = $i.Enabled
+        }
+    }
+}
+
+# Export the output to a CSV file
+$GpoLinks | Sort-Object Name | Export-Csv -Path "$FolderPath\GPO_Links.csv" -NoTypeInformation
---
+++ bulkusercrt.ps1
@@ -0,0 +1,52 @@
+﻿ # Import the PowerShell module for running AD cmdlets
+Import-Module activedirectory
+
+# Store the data from your CSV file in the $ADUsers variable
+$ADUsers = Import-Csv c:\bulkuser.csv
+#Enter the domain name to create the principle name with your domain
+$domain = "safnul.com"
+
+# Read each row of input data and assign each user’s details to variables
+foreach ($User in $ADUsers) {
+    $Username = $User.username
+    $Password = $User.password
+    $Firstname = $User.firstname
+    $Lastname = $User.lastname
+    $OU = $User.OU
+    $Email = $User.email
+    # $streetaddress = $User.address
+    # $city = $User.city
+    # $state = $User.state
+    # $country = $User.country
+    # $zipcode = $User.zipcode
+    # $telephone = $User.telephone
+    # $jobtitle = $User.title
+    # $company = $User.company
+    # $department = $User.department
+
+    # Check whether the user already exists in the AD 
+    if (Get-ADUser -Filter {SamAccountName -eq $Username}) {
+        # If the user already exists, display a warning 
+        Write-Warning "A user account with username $Username already exists in Active Directory."
+    } else {
+        # Otherwise, create the new user account in the specified OU
+        try {
+            New-ADUser -SamAccountName $Username `
+                       -UserPrincipalName "$Username@$domain" `
+                       -Name "$Firstname $Lastname" `
+                       -GivenName $Firstname `
+                       -Surname $Lastname `
+                       -Enabled $true `
+                       -DisplayName "$Firstname $Lastname" `
+                       -Path $OU `
+                       -EmailAddress $Email `
+                       -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
+                       -ChangePasswordAtLogon $true
+            
+            Write-Output "User account $Username has been created successfully."
+        } catch {
+            Write-Error "Failed to create user account $Username. Error: $_"
+        }
+    }
+}
+ 
